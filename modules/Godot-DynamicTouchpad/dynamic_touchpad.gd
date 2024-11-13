06b0b597d6018bab3c6db12878650c18c3249f81
David J. Allen <davidallendj@gmail.com>
2024-11-12 23:07:05 -0700

    module: improved Godot-DynamicTouchpad and added some options

--- modules/Godot-DynamicTouchpad/dynamic_touchpad.gd
+++ modules/Godot-DynamicTouchpad/dynamic_touchpad.gd
@@ -2,28 +2,37 @@ extends Control
 
 signal button_pressed(index: int)
 
+@export var hide_delay := 1.0				## Set the delay before hiding buttons.
+@export var enable_sticky_buttons := true	## Pressed buttons without sliding motion.
+@export var keep_visible := false			## Always show the other buttons.
 
 var _lmb_pressed := false
 var _button_index := -1
-var _enable_sticky_buttons := false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$start.button_down.connect(_toggle_view.bind(true))
-	$start.button_up.connect(_toggle_view.bind(false))
+	# set up callbacks for showing buttons dynamically
+	$timer.timeout.connect(_toggle_view.bind(false))
+	$action.button_down.connect(_toggle_view.bind(true))
+	$action.button_up.connect($timer.start.bind(hide_delay))
 	
 	# set button index to correct region
-	$start/up.mouse_entered.connect(_set_button_index.bind($start/up.get_index()))
-	$start/right.mouse_entered.connect(_set_button_index.bind($start/right.get_index()))
-	$start/down.mouse_entered.connect(_set_button_index.bind($start/down.get_index()))
-	$start/left.mouse_entered.connect(_set_button_index.bind($start/left.get_index()))
+	$action/up.mouse_entered.connect(_set_button_index.bind($action/up.get_index()))
+	$action/right.mouse_entered.connect(_set_button_index.bind($action/right.get_index()))
+	$action/down.mouse_entered.connect(_set_button_index.bind($action/down.get_index()))
+	$action/left.mouse_entered.connect(_set_button_index.bind($action/left.get_index()))
+	
+	# set whether to show the buttons initially
+	_toggle_view(keep_visible)
 
 
 func _toggle_view(is_pressed: bool) -> void:
-	$start/up.set_visible(is_pressed)
-	$start/right.set_visible(is_pressed)
-	$start/down.set_visible(is_pressed)
-	$start/left.set_visible(is_pressed)
+	if keep_visible:
+		is_pressed = true
+	$action/up.set_visible(is_pressed)
+	$action/right.set_visible(is_pressed)
+	$action/down.set_visible(is_pressed)
+	$action/left.set_visible(is_pressed)
 
 func _set_button_index(index: int) -> void:
 	_button_index = index
@@ -31,6 +40,12 @@ func _set_button_index(index: int) -> void:
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		_lmb_pressed = event.get_button_index() == MOUSE_BUTTON_LEFT and event.is_pressed()
+		
+		# handle pressing the left mouse button
+		if _lmb_pressed:
+			$timer.stop()
+		
+		# handle releasing the left mouse button
 		if event.get_button_index() == MOUSE_BUTTON_LEFT and event.is_released():
 			print("button index: ", _button_index)
 			button_pressed.emit(_button_index)
@@ -40,8 +55,10 @@ func _input(event: InputEvent) -> void:
 				1: $cursor.set_position($cursor.get_position() + Vector2(1, 0) * delta)
 				2: $cursor.set_position($cursor.get_position() + Vector2(0, 1) * delta)
 				3: $cursor.set_position($cursor.get_position() + Vector2(-1, 0) * delta)
-			if not _enable_sticky_buttons:
+			if not enable_sticky_buttons:
 				_button_index = -1
+			else:
+				$timer.start(hide_delay)
 	
 	#if _lmb_pressed and event is InputEventMouseMotion:
 		#$cursor.set_position(get_local_mouse_position())
